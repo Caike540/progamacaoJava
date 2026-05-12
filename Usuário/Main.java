d628f7773b3e67c9e56a76e2ed78a6e46a942d81
Caike540 <caikealmeida05@gmail.com>
2026-05-12 20:20:22 +0000

    commit

---
+++ Usuário/Main.java
@@ -0,0 +1,13 @@
+package Usuário;
+
+public class Main {
+    public static void main(String[] args) {
+        usuario u1 = new usuario();
+        u1.nome = "Fulano";
+        u1.email = "fulano@gmail.com";
+        u1.senha = "123456";
+
+        u1.fazerLogin();
+    }
+    
+}
---
+++ Usuário/usuario.java
@@ -0,0 +1,12 @@
+package Usuário;
+
+public class usuario {
+    String nome;
+    String email;
+    String senha;
+
+    public void fazerLogin() {
+        System.out.println(nome + " está tentando fazer login.");
+    }
+    
+}
