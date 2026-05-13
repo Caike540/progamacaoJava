5d67e1dfeb909ef6aae40e014da4ea8ad17a97a1
Caike540 <caikealmeida05@gmail.com>
2026-05-13 12:49:40 +0000

    commit

--- Introducao/Produto/Main.java
+++ Introducao/Produto/Main.java
@@ -3,7 +3,7 @@ package Introducao.Produto;
 public class Main {
     public static void main(String[] args) {
         Produto produto1 = new Produto("Notebook", 5600.90, 10);
-        Produto produto2 = new Produto("Smartfone", 1500.55, 20);
+        Produto produto2 = new Produto("Smartphone", 1500.55, 20);
         produto1.exibirInformacoes();
         produto2.exibirInformacoes();
     }
