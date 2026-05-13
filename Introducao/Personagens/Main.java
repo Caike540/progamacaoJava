868c1477c7c07e57cf1a0137f85b4b0b573ea0eb
Caike540 <caikealmeida05@gmail.com>
2026-05-13 14:20:06 +0000

    Criei Personagens

--- Introducao/Personagens/Main.java
+++ Introducao/Personagens/Main.java
@@ -3,7 +3,7 @@ package Introducao.Personagens;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Desafio Novato: Os Guardiões de Eldoria\n");
+        System.out.println("\nDesafio Novato: Os Guardiões de Eldoria\n");
 
         Personagens heroi1 = new Personagens("Artur", "Lanceiro", 1, 100, 10.2);
         Personagens heroi2 = new Personagens("Kiara", "Espadachin", 10, 200, 15.6);
